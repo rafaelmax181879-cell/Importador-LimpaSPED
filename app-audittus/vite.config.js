35d9b45d6c90de920b6becb62ea5d2d66338e95c
Rafael <rafael.max181879@gmail.com>
2026-02-24 09:02:48 -0300

    OK Vite correção

--- app-audittus/vite.config.js
+++ app-audittus/vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // <-- A linha que faz o Electron encontrar a tela
   build: {
-    chunkSizeWarningLimit: 3000, // Aumenta o limite de tolerância para 3MB e cala o aviso
+    chunkSizeWarningLimit: 3000, // Silencia o aviso amarelo
   }
 })
