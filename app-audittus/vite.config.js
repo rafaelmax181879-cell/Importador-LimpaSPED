39876757e27ca1615f08b6c6e1485e6fbf4a475f
Rafael <rafael.max181879@gmail.com>
2026-02-24 08:44:35 -0300

    Alteração lIMITE "vITE"

--- app-audittus/vite.config.js
+++ app-audittus/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ESSA É A LINHA MÁGICA PARA O .EXE FUNCIONAR!
+  build: {
+    chunkSizeWarningLimit: 3000, // Aumenta o limite de tolerância para 3MB e cala o aviso
+  }
 })
